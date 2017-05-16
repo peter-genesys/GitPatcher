d2eaa1bd112c978c5ca7e49c8d3a2b58b3077cc3
Peter Burgess <peter.a.burgess@gmail.com>
2017-05-17 00:39:52 +1000

    Updated patch SS-001_01
    Added dummy patch record for 'TRK-01_01'
    
    Modified   patch/feature/ss/SS-001/SS-001_01/install.sql

--- patch/feature/ss/SS-001/SS-001_01/install.sql
+++ patch/feature/ss/SS-001/SS-001_01/install.sql
@@ -22,6 +22,27 @@ SPOOL SS-001_01.log
 CONNECT &&SIMPLE_user/&&SIMPLE_password@&&database
 set serveroutput on;
 execute &&PATCH_ADMIN_user..patch_installer.patch_started( -
+  i_patch_name         => 'TRK-01_01' -
+ ,i_patch_type         => 'dummy' -
+ ,i_db_schema          => '&&SIMPLE_user' -
+ ,i_branch_name        => 'release/patchset/trk/TRK-01_01' -
+ ,i_tag_from           => 'TRK-01.01A' -
+ ,i_tag_to             => 'TRK-01.01B' -
+ ,i_supplementary      => '' -
+ ,i_patch_desc         => 'Full Tracker install' -
+ ,i_patch_componants   => '' -
+ ,i_patch_create_date  => '11-05-2014' -
+ ,i_patch_created_by   => 'Peter' -
+ ,i_note               => '' -
+ ,i_rerunnable_yn      => 'N' -
+ ,i_remove_prereqs     => 'N' -
+ ,i_remove_sups        => 'N' -
+ ,i_track_promotion    => 'Y'); 
+
+execute &&PATCH_ADMIN_user..patch_installer.patch_completed(i_patch_name  => 'TRK-01_01');
+
+
+execute &&PATCH_ADMIN_user..patch_installer.patch_started( -
   i_patch_name         => 'SS-001_01' -
  ,i_patch_type         => 'feature' -
  ,i_db_schema          => '&&SIMPLE_user' -
@@ -165,7 +186,8 @@ Show error;
 COMMIT;
 PROMPT Compiling objects in schema &&SIMPLE_user
 execute &&PATCH_ADMIN_user..patch_invoker.compile_post_patch;
-execute &&PATCH_ADMIN_user..patch_installer.patch_completed;
+--PATCH_ADMIN patches are likely to loose the session state of patch_installer, so complete using the patch_name parm.
+execute &&PATCH_ADMIN_user..patch_installer.patch_completed(i_patch_name  => 'SS-001_01');
 COMMIT;
 PROMPT 
 PROMPT install.sql - COMPLETED.
